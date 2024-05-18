42ca0b86fa2effbf811a07bf13d217d49f3f9e36
Andrews Cordolino Sobral <andrewssobral@gmail.com>
2024-05-18 22:07:58 +0200

    Added C++17 as requirement, updated compile script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
-rm -rf build && mkdir -p build && cd build
-cmake -DCMAKE_PREFIX_PATH=$LIBTORCH_HOME .. && make
-./dtt_test
-#cmake -DCMAKE_PREFIX_PATH=$LIBTORCH_HOME .. -G Xcode
-#open dtt.xcodeproj
+export LIBTORCH_HOME=/path/to/libtorch
+rm -rf build
+cmake -S . -B build -DCMAKE_PREFIX_PATH=$LIBTORCH_HOME
+cmake --build build --config Release
+./build/dtt_test
+
+# cmake -DCMAKE_PREFIX_PATH=$LIBTORCH_HOME .. -G Xcode
+# open dtt.xcodeproj
